4d66a8f0a1492ffd0b275957a5e0b4d8dc03391e
OscarTest <160587850+FohlinOscar@users.noreply.github.com>
2024-02-20 14:49:32 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eventivo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
